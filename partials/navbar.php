b1fd5a7ffaf6a66a49eb520282a8cbf8fb966034
parthvyas18896 <pvyas1@confederationcollege.ca>
2021-07-21 18:36:36 -0400

    fixed bugs

--- partials/navbar.php
+++ partials/navbar.php
@@ -33,7 +33,7 @@
                 <form class="form-inline my-2 my-lg-0">
                     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-success my-2 my-sm-0 mr-2" type="submit">Search</button>
-                    <a href='./auth/logout.php' class="btn btn-outline-danger my-2 my-sm-0">Logout</a>
+                    <a href='../auth/logout.php' class="btn btn-outline-danger my-2 my-sm-0">Logout</a>
                 </form>
             <?php else : ?>
                 <ul class="navbar-nav ml-auto">
