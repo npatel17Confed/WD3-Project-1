648ea02d397562b560015e66a0730b65b5485ff3
Chintan Trivedi <chintantrivedi247@gmail.com>
2021-07-21 15:35:31 -0400

    logout done

--- index.php
+++ index.php
@@ -36,8 +36,8 @@
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Logout</button>
+      <button class="btn btn-outline-success my-2 my-sm-0 mr-2" type="submit">Search</button>
+      <a href='./auth/logout.php' class="btn btn-outline-danger my-2 my-sm-0">Logout</a>
       
     </form>
   </div>
