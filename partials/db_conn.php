3527b780733968c57270690e05a31bf921c3694a
Chintan Trivedi <chintantrivedi247@gmail.com>
2021-07-22 18:07:02 -0400

    changed db name

--- partials/db_conn.php
+++ partials/db_conn.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost", "root", "", "wd3-project");
+    $con = mysqli_connect("localhost", "root", "", "wd3-project-1");
 
     if(mysqli_connect_errno()) {
         echo 'DB Connection Error';
