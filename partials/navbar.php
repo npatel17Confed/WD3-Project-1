4aa4cbfc3aa320d217f0ac3ec8c2680ab57dd349
Nilkumar Patel <nsp4898@gmail.com>
2021-07-22 12:57:44 -0400

    added minor changes and fixed bhuggs

--- partials/navbar.php
+++ partials/navbar.php
@@ -10,7 +10,7 @@
             <?php if (isset($_SESSION) && isset($_SESSION["id"])) : ?>
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="./../main/index.php">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="../main/index.php">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="../main/product.php">Products</a>
