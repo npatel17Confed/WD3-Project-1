8badc5ae8e235bbc9f170ab8e55bfc4926713284
parthvyas18896 <pvyas1@confederationcollege.ca>
2021-07-22 17:07:08 -0400

    added arriving and aboutus page

---
+++ main/aboutus.php
@@ -0,0 +1,16 @@
+<?php include '../partials/db_conn.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include '../partials/metadata.php'; ?>
+
+<?php include '../partials/navbar.php'; ?>
+
+<body>
+
+
+</body>
+
+
+</html>
